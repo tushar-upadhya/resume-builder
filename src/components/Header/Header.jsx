464dcb66d789ea8b4aeee93a74b182d451627470
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-05-11 18:11:47 +0530

    header bug fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,13 +8,13 @@ function Header() {
     return (
         <div className={styles.container}>
             {/* <div className={styles.left}>
-        <p className={styles.heading}>
-          A <span>Resume</span> that stands out!
-        </p>
-        <p className={styles.heading}>
-          Make your own resume. <span>It's free</span>
-        </p>
-      </div> */}
+                <p className={styles.heading}>
+                    A <span>Resume</span> that stands out!
+                </p>
+                <p className={styles.heading}>
+                    Make your own resume. <span>It's free</span>
+                </p>
+            </div> */}
             <div className={styles.right}>
                 <img src={resumeSvg} alt="Resume" />
             </div>
